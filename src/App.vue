b3935e033e37bc282131162c6dd8e77fb82a5a20
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-03-01 15:08:32 +0530

    modal is working

--- src/App.vue
+++ src/App.vue
@@ -18,13 +18,27 @@ export default {
 html,body{
   background-image: url('./assets/images/background_light.png');
   margin: 0;
+  background-color: #F13737;
 }
-/* #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-} */
+#app {
+  max-width: 100vw;
+  overflow: hidden;
+}
+* {
+  -webkit-touch-callout: none; /*系统默认菜单被禁用*/
+  -webkit-user-select: none; /*webkit浏览器*/
+  -khtml-user-select: none; /*早期浏览器*/
+  -moz-user-select: none; /*火狐*/
+  -ms-user-select: none; /*IE10*/
+  user-select: none;
+  color: #fff;
+}
+:root {
+  --van-swipe-indicator-active-background-color: #fff;
+  --van-swipe-indicator-inactive-background-color: #bababa;
+  --van-swipe-indicator-size: 8px;
+}
+.rtl_p {
+  direction: rtl;
+}
 </style>
--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -16,53 +16,62 @@
             </div> -->
 
             <div class="float_rules" @click="openRule">
-                <!-- <span :class="selectLanguageIndex == 5 ? ' mgaText' : ''">
-                    {{ btnTextList[selectLanguageIndex - 1] }}</span> -->
+                <span :class="selectLanguageIndex == 5 ? ' mgaText' : ''">
+                    {{ btnTextList[selectLanguageIndex - 1] }}</span>
             </div>
             <!-- howtoplay -->
-            <!-- <howToPlay :language="selectLanguageIndex" v-if="ruleShow" @closeMe="ruleShow = false">
-            </howtoplay> -->
+            <howToPlay :language="selectLanguageIndex" v-if="ruleShow" @closeMe="ruleShow = false">
+            </howtoplay>
         </div>
     </div>
 </template>
 
 <script>
 import { ref } from 'vue';
-// import howToPlay from './howToPlay.vue';
+import howToPlay from './howToPlay.vue';
 
 export default {
     name: 'HeaderComponent',
     setup() {
-        let language = ref(["English", "Urdu", "Hindi", "Bengali", "Filipino"]);
-        let ruleShow = ref(false);
-        let selectLanguageIndex = ref(1);
-        let activeKey = ref("");
+        // Define reactive variables
+        const language = ref(["English", "Urdu", "Hindi", "Bengali", "Filipino"]);
+        const ruleShow = ref(false);
+        const selectLanguageIndex = ref(1);
+        const activeKey = ref("");
+        let btnTextList = ref(["Rules", "قواعد", "नियम", "নিয়ম", "Mga tuntunin"]);
 
+
+        // Define function to open rule
         function openRule() {
             ruleShow.value = true;
             baseFun("rule");
         }
 
+        // Define base function
         function baseFun(location) {
-            let lang = language.value[selectLanguageIndex.value - 1];
-            console.log('lang...??',lang);
-            window.oyetalkActivityJsAnalysis.track("click_event", {
-                location,
-                module: window.location.href.split("?")[0],
-                name: activeKey.value,
-                language: lang,
-            });
+            const lang = language.value[selectLanguageIndex.value - 1];
+            // Assuming window.oyetalkActivityJsAnalysis is initialized somewhere else
+            if (window.oyetalkActivityJsAnalysis && window.oyetalkActivityJsAnalysis.track) {
+                window.oyetalkActivityJsAnalysis.track("click_event", {
+                    location,
+                    module: window.location.href.split("?")[0],
+                    name: activeKey.value,
+                    language: lang,
+                });
+            }
         }
 
         return {
             baseFun,
             openRule,
             language,
-            selectLanguageIndex
-        }
+            selectLanguageIndex,
+            ruleShow,
+            btnTextList
+        };
     },
-    components:{
-        // howToPlay,
+    components: {
+        howToPlay,
     }
 }
 </script>
@@ -70,16 +79,21 @@ export default {
 <style lang="less" scoped>
 .header {
     background-image: url('../assets/images/header.jpg');
-    width: 100%;
-    height: 100vh;
-    background-size: contain;
+    // width: 100%;
+    // height: 100vh;
+    // background-size: contain;
+    // background-repeat: no-repeat;
     background-repeat: no-repeat;
+    background-size: 100% 100%;
+    box-sizing: border-box;
+    height: 16rem;
+    position: absolute;
+    width: 100%;
 }
 
 .float_rules {
     position: absolute;
     background: linear-gradient(0deg, #733dfd, #e477ff);
-
     border: 2px solid #c4a1ff;
     border-radius: 9999px 0 0 9999px;
     width: 65px;
@@ -102,13 +116,10 @@ export default {
         outline: none;
         background: none;
     }
-
-    background-size: 100% 100%;
 }
 
 .modal {
     display: none;
-    /* Hide the modal by default */
     position: fixed;
     z-index: 1;
     left: 0;
@@ -116,7 +127,6 @@ export default {
     width: 100%;
     height: 100%;
     overflow: auto;
-    background-color: rgb(0, 0, 0);
     background-color: rgba(0, 0, 0, 0.4);
 
     .modal-content {
@@ -132,16 +142,16 @@ export default {
         float: right;
         font-size: 28px;
         font-weight: bold;
+
+        &:hover,
+        &:focus {
+            color: #000;
+            text-decoration: none;
+            cursor: pointer;
+        }
     }
 
-    .close:hover,
-    .close:focus {
-        color: #000;
-        text-decoration: none;
-        cursor: pointer;
-    }
-
-    .bilingual {
+    .bilingual { 
         position: absolute;
         width: 56px;
         background-color: #fff;
@@ -154,76 +164,76 @@ export default {
         padding-right: 20px;
         transform: scale(0.9);
         z-index: 40;
-    }
 
-    .language {
-        color: #000;
-        font-size: 12px;
-    }
+        .language {
+            color: #000;
+            font-size: 12px;
+        }
 
-    .rtbg {
-        position: absolute;
-        width: 21px;
-        height: 20px;
-        right: 0px;
-        top: 0px;
-        background: rgba(193, 104, 255, 1);
-        border-top-right-radius: 2em;
-        border-bottom-right-radius: 2em;
-    }
+        .rtbg {
+            position: absolute;
+            width: 21px;
+            height: 20px;
+            right: 0px;
+            top: 0px;
+            background: rgba(193, 104, 255, 1);
+            border-top-right-radius: 2em;
+            border-bottom-right-radius: 2em;
+        }
 
-    .arrow_down {
-        position: absolute;
-        right: 0.11rem;
-        top: 0.17rem;
-        cursor: pointer;
-        transition-duration: 0.2s;
-        width: 0;
-        height: 0;
-        border-width: 0.2rem 0.2rem 0;
-        border-style: solid;
-        border-color: #fff transparent transparent;
-    }
+        .arrow_down,
+        .arrow_up {
+            position: absolute;
+            right: 0.11rem;
+            top: 0.17rem;
+            cursor: pointer;
+            transition-duration: 0.2s;
+            width: 0;
+            height: 0;
+            border-width: 0.2rem 0.2rem 0;
+            border-style: solid;
+            border-color: #fff transparent transparent;
 
-    .arrow_up {
-        transform: rotate(180deg);
-        top: 0.14rem;
-    }
+            &.arrow_up {
+                transform: rotate(180deg);
+                top: 0.14rem;
+            }
+        }
 
-    .check {
-        width: 2.2rem;
-        left: 0;
-        bottom: 0;
-        position: absolute;
-        background-color: rgba(255, 255, 255, 0.8);
-        height: 0;
-        overflow: hidden;
-        transition-duration: 0.2s;
-        border-radius: 0.1rem;
-        z-index: 15;
-    }
+        .check {
+            width: 2.2rem;
+            left: 0;
+            bottom: 0;
+            position: absolute;
+            background-color: rgba(255, 255, 255, 0.8);
+            height: 0;
+            overflow: hidden;
+            transition-duration: 0.2s;
+            border-radius: 0.1rem;
+            z-index: 15;
 
-    span {
-        display: block;
-        width: 100%;
-        height: 0.64rem;
-        font-size: 0.24rem;
-        text-align: center;
-        line-height: 0.64rem;
-        border-bottom: 1px solid #fff;
-        color: #000;
-    }
+            span {
+                display: block;
+                width: 100%;
+                height: 0.64rem;
+                font-size: 0.24rem;
+                text-align: center;
+                line-height: 0.64rem;
+                border-bottom: 1px solid #fff;
+                color: #000;
 
-    span:last-child {
-        border-bottom: none;
-        box-sizing: border-box;
-    }
+                &:last-child {
+                    border-bottom: none;
+                    box-sizing: border-box;
+                }
+            }
+        }
 
-
-    .check_show {
-        height: 3.3rem;
-        bottom: -3.34rem;
-        z-index: 15;
+        &.check_show {
+            height: 3.3rem;
+            bottom: -3.34rem;
+            z-index: 15;
+        }
     }
 }
 </style>
