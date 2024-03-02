3c1f5e7f37dfc5d9437900b21653be5325e47fc7
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-03-02 16:05:35 +0530

    leaderboard strip has been completed

--- src/components/leaderBoardComp.vue
+++ src/components/leaderBoardComp.vue
@@ -1,6 +1,25 @@
 <template>
+    <div class="welcome-title">
+        <img src="../assets/images/picture_title.png" alt="welcom">
+    </div>
     <div class="leaderBoard">
-        <span class="text_14">Welcome&nbsp;to&nbsp;YAPA!&nbsp;Start&nbsp;your&nbsp;first&nbsp;topup&nbsp;with&nbsp;more&nbsp;suprising&nbsp;rewards!</span>
+        <span
+            class="text_14">Welcome&nbsp;to&nbsp;YAPA!&nbsp;Start&nbsp;your&nbsp;first&nbsp;topup&nbsp;with&nbsp;more&nbsp;suprising&nbsp;rewards!</span>
+        <div class="leaderBoard-strip">
+            <div class="beans-icon">
+                <img src="../assets/images/beans-icon.png" alt="beans">
+                <span>1000</span>
+            </div>
+
+            <div class="img-container">
+                <div class="left-icon">
+                    <img src="../assets/images/group-beans.png" alt="groupbeans">
+                </div>
+                <div class="right-icon">
+                    <img src="../assets/images/wallet.png" alt="groupbeans">
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -19,7 +38,7 @@ export default {
     margin: 0 auto;
     border-radius: 2vw;
     position: relative;
-    top: 9vw;
+    top: -20vw;
     // bottom: -10rem;
 }
 
@@ -36,8 +55,90 @@ export default {
     line-height: 20px;
     display: block;
     margin: 0 auto;
-    top: 3vw;
+    top: 9vw;
     // margin: 73px 0 0 34px;
     // margin: 0 auto;
 }
-</style>
+
+.welcome-title {
+    width: 70%;
+    height: 50%;
+    margin: 10vw auto;
+    position: relative;
+    z-index: 1;
+
+    img {
+        width: 100%;
+    }
+}
+
+.leaderBoard-strip {
+    background: url(../assets/images/strip.png);
+    height: 13vh;
+    width: 100%;
+    // opacity: 100;
+    background-size: cover;
+    background-size: 100% 100%;
+
+    // background-color: rgba(0, 10, 0, 100);
+}
+
+.beans-icon {
+    display: flex;
+    flex-direction: column;
+    width: 10%;
+    gap: 3vw;
+
+    img {
+        width: 100%;
+    }
+
+    span {
+        width: 10%;
+    }
+}
+
+
+.img-container {
+    display: flex;
+    justify-content: space-evenly;
+    position: relative;
+    top: -3.5rem;
+
+    .left-icon {
+        width: 20%;
+        height: 12vh;
+        background: url(../assets/images/icon-rectangle.png);
+        background-size: cover;
+        background-size: 100% 100%;
+
+        img {
+            width: 100%;
+        }
+    }
+
+    .right-icon {
+        width: 20%;
+        height: 12vh;
+        background: url(../assets/images/icon-rectangle.png);
+        background-size: cover;
+        background-size: 100% 100%;
+        img {
+            width: 90%;
+            margin: 0 auto;
+        }
+    }
+}
+
+
+
+
+// .leaderBoard-strip {
+//     content: "";
+//     // position: absolute;
+//     top: 0;
+//     left: 0;
+//     width: 100%;
+//     height: 100%;
+//     background-color: rgba(0, 0, 0, 0.5); /* Adjust opacity here (0.5 is 50% opacity) */
+// }</style>
